65191daaa2b82ec9a04597a0b8c7b75063d4561c
binduchinni <bindu@digitalrupay.com>
2019-02-13 15:28:57 +0530

    edited

--- CBL.java
+++ CBL.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 
 public class CBL {
   @Test
